adf6da6ad2f86311f76a3597a7f032b82db3f797
mari <m.m.04240816@gmail.com>
2025-04-06 12:09:04 +0900

    ドキュメントを参考にcssファイル作成

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vite.dev/config/
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    tailwindcss(),
+  ],
 })
