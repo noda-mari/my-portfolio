4768ea2c0c9a77d138515544b0b677159ddc2961
mari <m.m.04240816@gmail.com>
2025-04-08 13:13:55 +0900

    ページ遷移時のアニメーション

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
-  <router-view />
+  <Transition
+    enter-active-class="transition duration-300 ease-out"
+    enter-from-class="opacity-0 translate-x-5"
+    enter-to-class="opacity-100 translate-x-0"
+    leave-active-class="transition duration-200 ease-in"
+    leave-from-class="opacity-100 translate-x-0"
+    leave-to-class="opacity-0 -translate-x-5"
+    mode="out-in"
+  >
+    <RouterView />
+  </Transition>
 </template>
+
 
 <style scoped>
 
