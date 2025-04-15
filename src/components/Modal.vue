23ed4fe606c0b9f5a8fd21aa4fd98bd4851f4640
mari <m.m.04240816@gmail.com>
2025-04-15 16:41:54 +0900

    レスポンシブデザインに対応

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -2,14 +2,14 @@
   <transition name="fade">
     <div v-if="isOpen" class="fixed inset-0 z-50 flex items-center justify-center bg-[#361951] bg-opacity-80"
       @click.self="close">
-      <div class="relative bg-modal-bg rounded-lg shadow-lg p-6 w-full max-w-2xl">
-        <h2 class="text-xl text-black font-bold mb-4">{{ career?.title }}</h2>
+      <div class="relative bg-modal-bg rounded-lg shadow-lg p-6 sm:p-2 w-[90%] sm:w-full max-w-2xl">
+        <h2 class="text-xl text-black font-bold mb-4 text-center">{{ career?.title }}</h2>
         <div class="justify-items-start">
           <p v-for="(item, index) in career?.content" :key="index" class="text-black text-lg mb-3 text-left">
             {{ item }}
           </p>
         </div>
-        <button @click="close" class="absolute top-2 right-4 text-black text-3xl font-bold">×</button>
+        <button @click="close" class="absolute top-0.5 sm:top-2 right-2 sm:right-4 text-black text-3xl font-bold">×</button>
       </div>
     </div>
   </transition>
--- src/views/WorkDetail.vue
+++ src/views/WorkDetail.vue
@@ -1,24 +1,24 @@
 <template>
-  <div v-if="app" class="w-full px-10 pt-20 flex flex-col gap-6">
-    <h1 class="text-3xl font-bold mb-2 text-center">{{ app.title }}</h1>
-    <div class="grid grid-cols-6 gap-4">
-      <div class="col-span-3">
+  <div v-if="app" class="w-full px-4 md:px-8 lg:px-10 pt-20 flex flex-col gap-6">
+    <h1 class="text-2xl md:text-3xl font-bold mb-2 text-center">{{ app.title }}</h1>
+    <div class="grid grid-cols-1 lg:grid-cols-6 gap-4">
+      <div class="lg:col-span-3">
         <div class="flex flex-col gap-4">
           <img v-for="(image, index) in app.imageList" :key="index" :src="image" alt="アプリ画面"
             class="w-full max-w-md rounded border cursor-pointer" @click="openModal(index)" />
         </div>
       </div>
-      <div class="col-span-3 flex flex-col gap-6">
+      <div class="lg:col-span-3 flex flex-col gap-6">
         <p>製作期間：{{ app.duration }}</p>
         <div>
           <h2 class="text-lg font-bold">【概要】</h2>
-          <p class="w-3/4">{{ app.description }}</p>
+          <p class="w-full lg:w-3/4">{{ app.description }}</p>
         </div>
         <div v-if="app.gitHubUrl">
           <h2 class="text-lg font-bold">【Github URL】</h2>
           <a
             :href="app.gitHubUrl"
-            class="w-3/4 text-blue-400 hover:underline break-words"
+            class="w-full lg:w-3/4 text-blue-400 hover:underline break-words"
             target="_blank"
             rel="noopener noreferrer"
             >
@@ -33,7 +33,7 @@
         </div>
         <div>
           <h2 class="text-lg font-bold mb-2">【開発体制】</h2>
-          <div class="grid grid-cols-[10ch_1fr] gap-x-4 gap-y-2 text-sm">
+          <div class="grid grid-cols-[10ch_1fr] gap-x-4 gap-y-2 text-sm md:text-base">
             <div class="font-bold whitespace-nowrap">開発期間</div>
             <p>：{{ app.date }}</p>
 
@@ -49,7 +49,7 @@
         </div>
         <div>
           <h2 class="text-lg font-bold mb-2">【担当業務】</h2>
-          <div class="grid grid-cols-[10ch_1fr] gap-x-4 gap-y-2 text-sm">
+          <div class="grid grid-cols-[10ch_1fr] gap-x-4 gap-y-2 text-sm md:text-base">
             <div class="font-bold whitespace-nowrap">担当工程</div>
             <div>{{ app.inCharge }}</div>
 
@@ -59,9 +59,9 @@
             </ul>
           </div>
         </div>
-        <div>
+        <div class="mb-6">
           <h2 class="text-lg font-bold mb-2">【開発環境及び使用ツール】</h2>
-          <div class="grid grid-cols-[10ch_1fr] gap-x-4 gap-y-2 text-sm">
+          <div class="grid grid-cols-[10ch_1fr] gap-x-4 gap-y-2 text-sm md:text-base">
             <div class="font-bold whitespace-nowrap">使用技術一覧</div>
             <ul class="list-none">
               <li v-for="(tech, index) in app.techList" :key="index">・{{ tech }}</li>
